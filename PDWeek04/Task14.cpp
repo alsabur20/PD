f496eedc159d8c0f1298522ff15f1f1f62419289
ABDULSABUR <alsabur20@gmail.com>
2023-01-02 14:33:38 +0500

    changes made to task14

--- PDWeek04/Task14.cpp
+++ PDWeek04/Task14.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 void header();
 void printMenu();
+void aggregate1(string nameStd1,float matricMarksStd1,float interMarksStd1,float ecatMarksStd1,float aggregateStd1,string empty);
+void aggregate2(string nameStd2,float matricMarksStd2,float interMarksStd2,float ecatMarksStd2,float aggregateStd2,string empty);
+void compareAggregate(string nameStd1,float ecatMarksStd1,string nameStd2,float ecatMarksStd2,string empty);
 
 main()
 {
@@ -74,7 +77,48 @@ while(true)
   if(choice == 3)
   {
     system("cls");
-    aggregateStd1 = (matricMarksStd1/1100*100)*0.3 + (interMarksStd1/550*100)*0.3 + (ecatMarksStd1/400*100)*0.4 ;
+    cout << "You chose: " << choice << endl;
+    aggregate1(nameStd1,matricMarksStd1,interMarksStd1,ecatMarksStd1,aggregateStd1,empty);
+  }
+  if(choice == 4)
+  {
+    system("cls");
+    cout << "You chose: " << choice << endl;
+    aggregate2(nameStd2,matricMarksStd2,interMarksStd2,ecatMarksStd2,aggregateStd2,empty);
+  }
+  if(choice == 5)
+  {
+   cout << "You chose: " << choice << endl;
+   compareAggregate(nameStd1,ecatMarksStd1,nameStd2,ecatMarksStd2,empty);
+  }
+}
+}
+
+void header()
+{
+ cout << "################" << endl;
+ cout << "#  UET LAHORE  #" << endl;
+ cout << "################" << endl;
+ cout << endl;
+ cout << endl;
+ cout << endl;
+ cout << "Welcome to UET Admission Management System";
+ cout << endl;
+ cout << endl;
+ cout << endl;
+}
+
+void printMenu()
+{
+ cout << "Press 1 to Enter Details of First Students!" << endl;
+ cout << "Press 2 to Enter Details of Second Students!" << endl;
+ cout << "Press 3 to calculate aggregate of first students!" << endl;
+ cout << "Press 4 to calculate aggregate of second students!" << endl;
+ cout << "Press 5 to display the student with RollNumber 01!" << endl;
+}
+
+void aggregate1(string nameStd1,float matricMarksStd1,float interMarksStd1,float ecatMarksStd1,float aggregateStd1,string empty)
+{    aggregateStd1 = (matricMarksStd1/1100*100)*0.3 + (interMarksStd1/550*100)*0.3 + (ecatMarksStd1/400*100)*0.4 ;
     cout << nameStd1 << "'s aggregate is " << aggregateStd1 << endl;
     cout << "Press any key to continue...    ";
     cin >> empty;
@@ -84,10 +128,10 @@ while(true)
     header();
     printMenu();
    }
-  }
-  if(choice == 4)
-  {
-    system("cls");
+}
+
+void aggregate2(string nameStd2,float matricMarksStd2,float interMarksStd2,float ecatMarksStd2,float aggregateStd2,string empty)
+{
     aggregateStd2 = (matricMarksStd2/1100*100)*0.3 + (interMarksStd2/550*100)*0.3 + (ecatMarksStd2/400*100)*0.4 ;
     cout << nameStd2 << "'s aggregate is " << aggregateStd2 << endl;
     cout << "Press any key to continue...    ";
@@ -98,9 +142,10 @@ while(true)
     header();
     printMenu();
    }
-  }
-  if(choice == 5)
-  {
+}
+
+void compareAggregate(string nameStd1,float ecatMarksStd1,string nameStd2,float ecatMarksStd2,string empty)
+{
 
     if (ecatMarksStd1 > ecatMarksStd2)
     {
@@ -132,30 +177,5 @@ while(true)
       printMenu();
      }
     }
-  }
-}
-}
 
-void header()
-{
- cout << "################" << endl;
- cout << "#  UET LAHORE  #" << endl;
- cout << "################" << endl;
- cout << endl;
- cout << endl;
- cout << endl;
- cout << "Welcome to UET Admission Management System";
- cout << endl;
- cout << endl;
- cout << endl;
 }
-
-void printMenu()
-{
- cout << "Press 1 to Enter Details of First Students!" << endl;
- cout << "Press 2 to Enter Details of Second Students!" << endl;
- cout << "Press 3 to calculate aggregate of first students!" << endl;
- cout << "Press 4 to calculate aggregate of second students!" << endl;
- cout << "Press 5 to display the student with RollNumber 01!" << endl;
-}
-
